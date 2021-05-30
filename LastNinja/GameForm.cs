b301be17db9a06f2cec2a75dec3fa7d3d3c33655
qwqw ffe <misha.torno@gmail.com>
2021-05-30 15:00:55 +0500

    Add project files.

---
+++ LastNinja/GameForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace LastNinja
+{
+    public class GameForm:Form
+    {
+        public GameForm()
+        {
+            MakeForm();
+        }
+
+        private void MakeForm()
+        {
+            DoubleBuffered = true;
+            ClientSize = new Size(1600, 800);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            BackColor = Color.Bisque;
+        }
+    }
+}
---
+++ LastNinja/Map.cs
@@ -0,0 +1,7 @@
+﻿namespace LastNinja
+{
+    public class Map
+    {
+
+    }
+}
