b3f03565060c0b7e3537958cb7327d97b53327e2
qwqw ffe <misha.torno@gmail.com>
2021-06-02 23:08:40 +0500

    написал тесты на сюрикен

--- LastNinja/Game/Entities/Suriken.cs
+++ LastNinja/Game/Entities/Suriken.cs
@@ -4,7 +4,7 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
-        public (int Dx, int Dy) Size { get; }
+        public (int Dx, int Dy) Size { get; set; }
         public int Health { get; set; }
         public bool IsWorking { get; set; } = true;
 
---
+++ LastNinjaTests/tests/SurikenTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using LastNinja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LastNinja.Tests
+{
+    [TestFixture]
+    public class SurikenTests
+    {
+        private const int Speed = 20;
+
+        [TestCase(100, 100, 20, 20, Direction.Down, 20, 20 + Speed)]
+        [TestCase(100, 100, 20, 20, Direction.Left, 20 - Speed, 20)]
+        [TestCase(100, 100, 21, 20, Direction.Left, 21 - Speed, 20)]
+        [TestCase(100, 100, 20, 10, Direction.Up, 20, 10)]
+        [TestCase(100, 100, 80, 80, Direction.Down, 80, 80)]
+        public void MoveTest(int width, int height, int x, int y, Direction direction, int expectedX, int expectedY)
+        {
+            var map = new Map(width, height);
+            var player = new Player(map) {X = x, Y = y, Direction = direction, Size = (0, 0)};
+            var suriken = new Suriken(map, player) {Size = (0, 0)};
+
+            suriken.Move();
+
+            suriken.X.Should().Be(expectedX);
+            suriken.Y.Should().Be(expectedY);
+        }
+    }
+}
